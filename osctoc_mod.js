47a704abe580925ae802a5242b36976df7c24021
Osctoc571 <osctoc.workingfor@gmail.com>
2026-05-27 22:40:15 -0400

    Create osctoc_mod.js

---
+++ osctoc_mod.js
@@ -0,0 +1,22 @@
+G.AddData({
+name:'Example mod',
+author:'Os',
+desc:'A simple example mod that adds hot peppers and hot sauce.',
+engineVersion:1,
+manifest:'modManifest.js',
+requires:['Default dataset*'],
+func:function()
+{
+
+	new G.Res({
+		name:'test_food',
+		desc:'Test_food',
+		icon:0,
+		turnToByContext:{'eat':{'health':0.01,'happiness':0.03},'decay':{'spoiled food':0.5}},
+		partOf:'food',
+		category:'food',
+	});
+
+	G.getDict('grass').res['gather']['test_food']=10;
+}
+});
