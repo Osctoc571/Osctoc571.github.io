4143c01095f16df8580261b71892a7c336754b3e
Osctoc571 <osctoc.workingfor@gmail.com>
2026-05-28 22:57:02 -0400

    Update osctoc_mod.js

--- osctoc_mod.js
+++ osctoc_mod.js
@@ -8,15 +8,6 @@ requires:['Default dataset*'],
 sheets:{'iconSheet':'https://osctoc571.github.io/Iconsheet_osctoc.png'},
 func:function()
 {
-	G.funcs['new game blurb']=function()
-		{
-			var str=
-			'<b>Your tribe :</b><div class="thingBox">'+
-			'</div>'+
-			'<div class="par fancyText bitBiggerText">Your tribe finds a place to settle in the wilderness.<br>Resources are scarce, and everyone starts foraging.</div>'+
-			'<div class="par fancyText bitBiggerText">You emerge as the tribe\'s leader. They call you :</div>';
-			return str;
-		}
-
 	delete G.getDict('grass').res.gather['food'];
+}
 });
