fa5ca6b2f70c2414dbb95dd3ab0ec74c2cdc6fe9
Osctoc571 <osctoc.workingfor@gmail.com>
2026-05-28 22:30:53 -0400

    Update osctoc_mod.js

--- osctoc_mod.js
+++ osctoc_mod.js
@@ -4,7 +4,6 @@ author:'Os',
 desc:'A mod',
 engineVersion:1,
 manifest:'https://osctoc571.github.io/osctoc_mod.js',
-requires:['Default dataset*'],
 sheets:{'iconSheet':'https://osctoc571.github.io/Iconsheet_osctoc.png'},
 func:function()
 {
